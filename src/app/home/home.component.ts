524473f9eb5dd521494049272578441b2c9245bd
Langi <mlnp3@umsl.edu>
2017-08-13 20:57:15 -0500

    done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,7 +10,14 @@ export class HomeComponent implements OnInit {
     logo="../../assets/img/Webp.net-resizeimage (1).png";
   sLogo="../../assets/img/Webp.net-resizeimage.png";
   umslIS="../../assets/img/information_systems_logo.jpg"
-
+  og_UMSLHACK = "../../assets/img/og_logo.png"
+  umslHack1="../../assets/img/IMG_0011.JPG"
+  umslHack2 = "../../assets/img/IMG_9836.JPG"
+  umslHack3 = "../../assets/img/IMG_9887.JPG"
+  umslHack4 = "../../assets/img/DSC04230.JPG"
+  umslHack5 = "../../assets/img/IMG_9878.JPG"
+  umslHack6 = "../../assets/img/IMG_9967.JPG"
+  gotAPI=  "../../assets/img/test5.png"
   constructor() { }
 
   ngOnInit() {
