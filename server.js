185b4555e677d3b49fe96c7269b2650d0338bb81
Mohamed Langi <mlnp3@mail.umsl.edu>
2018-02-25 15:24:51 -0600

    Remove Timer

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
